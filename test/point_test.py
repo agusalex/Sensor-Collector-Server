4a0841b164b53c6402b17a65e2c326e703728d44
Pedro Gutierrez <pedrog4747@gmail.com>
2019-05-29 15:53:38 -0300

    Changed distance test to be easier

--- test/point_test.py
+++ test/point_test.py
@@ -6,9 +6,9 @@ from src.point import *
 class TestStringMethods(unittest.TestCase):
 
     def test_distance(self):
-        d = distance(Point(0, 1), Point(2, 3))
+        d = distance(Point(0, 8), Point(3, 4))
         print(d)
-        self.assertEqual(d, 278.54558935106695)
+        self.assertEqual(d, 5)
 
     def test_shift(self):
         p1 = Point(0, 1)
