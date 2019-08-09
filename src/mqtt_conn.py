63781e8624bf7fac7154a936a776b178e89e31f3
Pedro Gutierrez <pedrog4747@gmail.com>
2019-08-09 18:53:14 -0300

    Added mqtt connector

---
+++ src/mqtt_conn.py
@@ -0,0 +1,30 @@
+import ssl
+import sys
+
+import paho.mqtt.client
+
+
+def on_connect(client, userdata, flags, rc):
+    print('connected (%s)' % client._client_id)
+    client.subscribe(topic='arduino', qos=2)
+
+
+def on_message(client, userdata, message):
+    print('------------------------------')
+    print('topic: %s' % message.topic)
+    print('payload: %s' % message.payload)
+    print('qos: %d' % message.qos)
+
+
+def main():
+    client = paho.mqtt.client.Client(client_id='triangulation-subs', clean_session=False)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(host='127.0.0.1', port=1883)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
+
+sys.exit(0)
