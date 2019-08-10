bc128eba253a70b953bd71d2f417752d3adce4b3
Pedro Gutierrez <pedrog4747@gmail.com>
2019-08-09 23:27:45 -0300

    Connected parser and mqtt

--- src/mqtt_conn.py
+++ src/mqtt_conn.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+from src.parser import parse_and_save
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -13,6 +14,7 @@ def on_connect(client, userdata, rc):
 def on_message(client, userdata, msg):
     print("We got a message!")
     print('%s %s' % (msg.topic, msg.payload))
+    parse_and_save(msg.payload)
 
 
 client = mqtt.Client(client_id='triangulation-subs', clean_session=False)
@@ -27,3 +29,38 @@ client.connect(host='127.0.0.1', port=1883)
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 client.loop_forever()
+
+
+
+
+# import sys
+#
+# import paho.mqtt.client
+# from src.parser import parse_and_save
+#
+#
+# def on_connect(client, userdata, flags, rc):
+#     print('connected (%s)' % client._client_id)
+#     client.subscribe(topic='arduino', qos=2)
+#
+#
+# def on_message(client, userdata, message):
+#     print('------------------------------')
+#     print('topic: %s' % message.topic)
+#     print('payload: %s' % message.payload)
+#     print('qos: %d' % message.qos)
+#     parse_and_save(message.payload)
+#
+#
+# def main():
+#     client = paho.mqtt.client.Client(client_id='triangulation-subs', clean_session=False)
+#     client.on_connect = on_connect
+#     client.on_message = on_message
+#     client.connect(host='127.0.0.1', port=1883)
+#     client.loop_forever()
+#
+#
+# if __name__ == '__main__':
+#     main()
+#
+# sys.exit(0)
--- src/parser.py
+++ src/parser.py
@@ -6,8 +6,7 @@ from src.type import Type
 from src.db_utils import db_interact
 from datetime import datetime
 
-jsondir = '../data/sample.json'
-csvdir = '../data/mac-vendors.csv'
+csv_dir = '../data/mac-vendors.csv'
 
 
 def get_oui(input_mac):
@@ -17,36 +16,40 @@ def get_oui(input_mac):
 
 
 def get_vendor(input_mac):
-    reader = csv.reader(open(csvdir), delimiter=',', quotechar='|')
+    reader = csv.reader(open(csv_dir), delimiter=',', quotechar='|')
     device_oui = get_oui(input_mac)
     for row in reader:
         if device_oui == row[0]:
             return row[1]
 
 
-with open(jsondir, 'r') as fp:
-    obj = json.load(fp)
+def parse_and_save(received_json):
+    try:
+        obj = json.loads(received_json)
+    except ValueError as e:
+        print(str(e))
+        return
 
-macs = obj["MAC"]
-decibs = obj["RSSI"]
-milis = obj["MILIS"]
-ssids = obj["STATION/SSID"]
-types = obj["TYPE"]
-channels = obj["CH"]
+    macs = obj["MAC"]
+    decibs = obj["RSSI"]
+    milis = obj["MILIS"]
+    ssids = obj["STATION/SSID"]
+    types = obj["TYPE"]
+    channels = obj["CH"]
 
-type_c = Type('C')
-type_b = Type('B')
-type_r = Type('R')
+    type_c = Type('C')
+    type_b = Type('B')
+    type_r = Type('R')
 
-for i in range(len(macs)):
-    db_interact.open_session()
-    if types[i] == 'C':
-        db_interact.persist(Packet(datetime.fromtimestamp(milis[i]), decibs[i], macs[i], channels[i], ssids[i], type_c))
-    elif types[i] == 'B':
-        db_interact.persist(Packet(datetime.fromtimestamp(milis[i]), decibs[i], macs[i], channels[i], ssids[i], type_b))
-    else:
-        db_interact.persist(Packet(datetime.fromtimestamp(milis[i]), decibs[i], macs[i], channels[i], ssids[i], type_r))
-    db_interact.close_session()
-    print(milis[i], decibs[i], macs[i], channels[i], ssids[i], types[i])
-    print(get_vendor(macs[i]))
+    for i in range(len(macs)):
+        db_interact.open_session()
+        if types[i] == 'C':
+            db_interact.persist(Packet(datetime.fromtimestamp(milis[i]), decibs[i], macs[i], channels[i], ssids[i], type_c))
+        elif types[i] == 'B':
+            db_interact.persist(Packet(datetime.fromtimestamp(milis[i]), decibs[i], macs[i], channels[i], ssids[i], type_b))
+        else:
+            db_interact.persist(Packet(datetime.fromtimestamp(milis[i]), decibs[i], macs[i], channels[i], ssids[i], type_r))
+        db_interact.close_session()
+        print(milis[i], decibs[i], macs[i], channels[i], ssids[i], types[i])
+        print(get_vendor(macs[i]))
 
